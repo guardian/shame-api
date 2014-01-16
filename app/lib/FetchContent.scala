32ed64e48bf07223a1764197aebb8b74860a4f7e
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-01-16 16:48:39 +0000

    Output json and add webUrl. Todo Fix thumbnail for content that does not have thumbnail

--- app/lib/FetchContent.scala
+++ app/lib/FetchContent.scala
@@ -6,28 +6,34 @@ import com.gu.openplatform.contentapi.connection.HttpResponse
 
 import play.api.libs.ws.WS
 import scala.concurrent.{ExecutionContext, Future}
+import ExecutionContext.Implicits.global
 
-case class Shame(webTitle:String, standfirst: String, thumbnail: String)
-
-
-class FetchContent {
+case class Shame(webTitle:String, webUrl: String, standfirst: String, thumbnail: String)
 
 
-  def getShameWall(keywords: List[String])(implicit executionContext: ExecutionContext): Future[List[Shame]] = {
+object FetchContent {
 
-    val testKeywords = List("harry styles", "fifty shades of grey")
+  def getShameWall : Future[List[Shame]] = {
+    //get keywords -- todo
+    val testKeywords = List("harry styles")
+    getContent(testKeywords)
 
-    val results = Future.sequence(testKeywords.map { keyword =>
+  }
+
+  def getContent(keywords: List[String]): Future[List[Shame]] = {
+    val results = Future.sequence(keywords.map { keyword =>
       ApiClient.tags.q(keyword).response flatMap { tagsResponse =>
         val tags = tagsResponse.results
         val content = if(tags.nonEmpty) {
-          ApiClient.item.itemId(tags(0).id).showElements("image").response map { itemResponse =>
-            itemResponse.results.headOption.map(r => Shame(r.webTitle, r.fields.get("standfirst"), r.fields.get("thumbnail")))
+          ApiClient.item.itemId(tags(0).id).showElements("image").showFields("standfirst,thumbnail").response map { itemResponse =>
+            itemResponse.results.headOption.map(r =>
+              Shame(r.webTitle, r.webUrl, r.fields.get("standfirst"), r.fields.get("thumbnail")))
           }
         }
         else {
-          ApiClient.search.q(keyword).response map { searchResponse =>
-            searchResponse.results.headOption.map(r => Shame(r.webTitle, r.fields.get("standfirst"), r.fields.get("thumbnail")))
+          ApiClient.search.q(keyword).showElements("image").showFields("all").response map { searchResponse =>
+            searchResponse.results.headOption.map(r =>
+              Shame(r.webTitle, r.webUrl, r.fields.get("standfirst"), r.fields.get("thumbnail")))
           }
         }
         content
@@ -41,14 +47,13 @@ class FetchContent {
 
 object ApiClient extends FutureAsyncApi {
 
-  implicit val executionContext = scala.concurrent.ExecutionContext.global
-
-
   override protected def fetch(url: String, parameters: scala.collection.immutable.Map[String, String]) =
     super.fetch(url, parameters + ("api-key" -> "jenny"))
 
 
   def GET(urlString: String, headers: Iterable[(String, String)] = Nil): Future[HttpResponse] =
     WS.url(urlString).withHeaders(headers.toSeq: _*).get().map(r => HttpResponse(r.body, r.status, r.statusText))
+
+  implicit def executionContext: ExecutionContext = ExecutionContext.Implicits.global
 
 }
