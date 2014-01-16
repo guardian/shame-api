d5cc40778695467243675e6dcc68a0f58190b062
adamnfish <adamnfish@gmail.com>
2014-01-16 17:41:43 +0000

    Add jsonp support to JSON endpoint

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,19 +5,20 @@ import lib.{Shame, FetchContent}
 import play.api.libs.json._
 import scala.concurrent.ExecutionContext
 import ExecutionContext.Implicits.global
+import play.api.libs.Jsonp
 
 
 object Application extends Controller {
 
   implicit val shame = Json.format[Shame]
 
-  def index = Action.async {
+  def index = Action.async { implicit request =>
     FetchContent.getShameWall.map { shames =>
-      Ok(Json.toJson(shames))
-
+      request.getQueryString("callback").map { callback =>
+        Ok(Jsonp(callback, Json.toJson(shames)))
+      }.getOrElse {
+        Ok(Json.toJson(shames))
+      }
     }
   }
-
-
-
 }
