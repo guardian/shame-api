e65dd22f607035ae6b195e90135bbdf6c3dc0967
Robert Berry <rjberry@gmail.com>
2014-01-17 12:55:02 +0000

    Prefetch content

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,24 +1,23 @@
 package controllers
 
 import play.api.mvc._
-import lib.{Shame, FetchContent}
+import lib.{Agent, Shame, FetchContent}
 import play.api.libs.json._
 import scala.concurrent.ExecutionContext
 import ExecutionContext.Implicits.global
 import play.api.libs.Jsonp
 
-
 object Application extends Controller {
 
   implicit val shame = Json.format[Shame]
 
-  def index = Action.async { implicit request =>
-    FetchContent.getShameWall.map { shames =>
-      request.getQueryString("callback").map { callback =>
-        Ok(Jsonp(callback, Json.toJson(shames)))
-      }.getOrElse {
-        Ok(Json.toJson(shames))
-      }
+  def index = Action { implicit request =>
+    val shames = Agent.shameWall
+
+    request.getQueryString("callback").map { callback =>
+      Ok(Jsonp(callback, Json.toJson(shames)))
+    }.getOrElse {
+      Ok(Json.toJson(shames))
     }
   }
 }
